91fe89ef41b94240126309a8c2b30698b1bb7ec9
Hamziniii <hamzagaziuddin04@gmail.com>
2021-01-02 07:02:37 -0600

    Trees works, however the spacing is weird Fixes #2

--- commands.ts
+++ commands.ts
@@ -8,7 +8,31 @@ export class Bot {
     static test: MachinaFunction = (params: MachinaFunctionParameters) => {
         params.msg.reply('\n' + createTree("TEST", {
             "Prop 2": "Property 2",
-            prop3: "Prop3"
+            prop2: {
+                prop1: "23",
+                prop2: "21",
+                prop: {
+                    prop1: "23",
+                    prop2: "21",
+                    prop: {
+                        prop1: "23",
+                        prop2: "21" 
+                    }, 
+                }, 
+            },
+            prop3: "Prop3",
+            prop: {
+                prop1: "23",
+                prop2: "21" 
+            },
+            prop4: {
+                prop41: "1",
+                prop42: "2",
+                prop43: "4",
+                prop44: {
+                    prop441: "cheese"
+                }
+            }
         }))
     }
 }
--- util/ascii_tree.ts
+++ util/ascii_tree.ts
@@ -1,10 +1,15 @@
 import { arrify } from "machina.ts";
 import { AsciiTree } from "oo-ascii-tree";
+import { isTypeReferenceNode } from "typescript";
 
 /**
  * Creates an ascii tree when provided with a title and children
  * @param title THe title of the tree
  * @param children The children of the tree
+ * @param root Is this the root
  * @todo #3
  */
-export const createTree = (title: string, children: {[key: string]: string}) => (new AsciiTree(`${title}`, ...Object.keys(children).map(key => new AsciiTree(`${key}: ${children[key]}`)))).toString()
+export const createTree = (title: string, children: Object, root = true) => {
+    const tree: AsciiTree = new AsciiTree(title, ...Object.keys(children).map(key => typeof children[key] == "string" ? new AsciiTree(children[key]) : createTree(key, children[key], false) as AsciiTree))
+    return root ? tree.toString() : tree
+}
